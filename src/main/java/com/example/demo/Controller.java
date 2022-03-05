152a06f37cdb8da361b8bbd7cf7777531e2878f1
frankliu197 <frankliu197@gmail.com>
2022-03-05 15:12:52 -0500

    Template with question and answer model and h2 set up

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.demo.model.Question;
+import com.example.demo.repository.QuestionsRepository;
+
+
+@RestController
+class Controller {
+	
+	@Autowired
+	private QuestionsRepository questionsRepository;
+
+	@GetMapping("/question")
+	 List<Question> get() {
+		return questionsRepository.findAll();
+	}	
+}
---
+++ src/main/java/com/example/demo/model/Answer.java
@@ -0,0 +1,47 @@
+package com.example.demo.model;
+
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+
+public class Answer {
+	private @Id @GeneratedValue int id;	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id")
+	private Question question;
+	
+	private String answer;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+}
